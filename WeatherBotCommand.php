13d5e6cb8e3c2fe5eadef0229932c746238d4c85
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-07 23:45:20 +0800

    Initial implementation for the weather command.

--- WeatherBotCommand.php
+++ WeatherBotCommand.php
@@ -6,6 +6,8 @@ class WeatherBotCommand extends BotCommand {
     }
 
     protected function executeCommand($parameter) {
-        return $this->getCommand()." command not yet implemented. Parameter passed: ".$parameter;
+        $acResult = json_decode(file_get_contents('http://autocomplete.wunderground.com/aq?query='.urlencode($parameter)), true);
+        $condResult = json_decode(file_get_contents('http://api.wunderground.com/api/ec23707d4592d0cb/conditions/q/zmw:'.urlencode($acResult['RESULTS'][0]['zmw'].'.json')), true);
+        return 'Displaying current weather conditions in '.$condResult['current_observation']['display_location']['full'];
     }
 }
