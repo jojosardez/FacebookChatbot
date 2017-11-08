9aad6f1d7b76f3d51f2370c7e1c50c52211426d2
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-08 23:53:44 +0800

    Added button for viewing full forecast.

--- WeatherBotCommand.php
+++ WeatherBotCommand.php
@@ -7,17 +7,24 @@ class WeatherBotCommand extends BotCommand {
 
     protected function executeCommand($parameter) {
         $acResult = json_decode(file_get_contents('http://autocomplete.wunderground.com/aq?query='.urlencode($parameter)), true);
-        $condResult = json_decode(file_get_contents('http://api.wunderground.com/api/ec23707d4592d0cb/conditions/q/zmw:'.urlencode($acResult['RESULTS'][0]['zmw'].'.json')), true);
+        $zmwCode = $acResult['RESULTS'][0]['zmw'];
+        $condResult = json_decode(file_get_contents('http://api.wunderground.com/api/ec23707d4592d0cb/conditions/q/zmw:'.urlencode($zmwCode.'.json')), true);
         return ["attachment"=>[
             "type"=>"template",
             "payload"=>[
               "template_type"=>"generic",
               "elements"=>[
                 [
-                  "title"=>'Displaying current weather conditions in '.$condResult['current_observation']['display_location']['full'],
-                  "item_url"=>$condResult['current_observation']['forecast_url'],
+                  "title"=>'Current weather conditions in '.$condResult['current_observation']['display_location']['full'],
                   "image_url"=>$condResult['current_observation']['icon_url'],
-                  "subtitle"=>'Weather: '.$condResult['current_observation']['weather'].', Temperature: '.$condResult['current_observation']['temperature_string'].', Humidity: '.$condResult['current_observation']['relative_humidity'].', '.$condResult['current_observation']['observation_time'].'.'
+                  "subtitle"=>$condResult['current_observation']['weather'].' at '.$condResult['current_observation']['temperature_string'].'. '.$condResult['current_observation']['observation_time'].'.',
+                  "buttons"=>[
+                    [
+                      "type"=>'web_url',
+                      "url"=>'http://www.wunderground.com/q/zmw:'.$zmwCode,
+                      "title"=>"View Full Forecast"
+                    ]
+                  ]
                 ]
               ]
             ]
