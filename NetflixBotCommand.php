9fd2fbdce6ef9eb5f372d8bb9b2f60ba285af3e9
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-10 01:22:48 +0800

    Contains initial implementation for netflix command.

--- NetflixBotCommand.php
+++ NetflixBotCommand.php
@@ -6,6 +6,20 @@ class NetflixBotCommand extends BotCommand {
     }
 
     protected function executeCommand($parameter) {
-        $this->send($this->command." command is not yet implemented, ".$this->user->getFirstName().". Parameter passed: ".$parameter);
+        $this->send('getting response from instantwatcher');
+        $htmlResult = file_get_contents('http://instantwatcher.com/search?content_type=1+2&source=1+2+3&q='.urlencode($parameter));
+        $doc = new DOMDocument;
+        $doc->preserveWhiteSpace = false;
+        $doc->loadHTML($htmlResult);
+        $xpath = new DOMXpath($doc);
+        $this->send('response retrieved. performing xpath');
+        $queryResult = $xpath->query('//div[@class="iw-title netflix-title list-title box-synopsis-mode"]');
+        $this->send('xpath complete');
+        foreach ($queryResult as $queryItem) {
+            $title = $queryItem->xpath("./span[@class='title']/a");
+            $this->send((string)$title);
+            $year = $queryItem->xpath("./span[@class='year']/a");
+            $this->send((string)$year);
+        }
     }
 }
