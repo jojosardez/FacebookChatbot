cff46af2fca6e0935b3ba888746dbcf7426ece82
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-10 22:38:47 +0800

    Netflix command implementation - contains dealing with no parameter, no match, 1 match, less than or equal to 4 matches, and more than 4 matches.

--- NetflixBotCommand.php
+++ NetflixBotCommand.php
@@ -7,7 +7,7 @@ class NetflixBotCommand extends BotCommand {
 
     protected function executeCommand($parameter) {   
         if (trim($parameter) == "") {
-            $this->send("Sorry ".$this->user->getFirstName().", you need to specify the movie or series title that you want to search.");
+            $this->send("Sorry ".$this->user->getFirstName().", you need to specify the Netflix movie or series title that you want to search.");
             return;
         }
 
@@ -28,39 +28,53 @@ class NetflixBotCommand extends BotCommand {
         $result = json_decode(file_get_contents('https://unogs.com/nf.cgi?u=vgrv6hjhe2tlajqj6o7cjnhe71&q='.urlencode($parameter)."&t=ns&cl=&st=bs&ob=&p=1&l=100&inc=&ao=and", false, $headerContext), true);
 
         if ($result['COUNT'] == 0) {
-            $this->send("No movie or series found with the title \"".$parameter."\", ".$this->user->getFirstName().". The movie or series you're looking for is not in Netflix.");
+            $this->send("No movie or series found with the title \"".$parameter."\", ".$this->user->getFirstName().". The movie or series you're looking for is not in Netflix.");            
         }
         else {
-            $this->send($result['ITEMS'][0][1]." (".$result['ITEMS'][0][6].", ".$result['ITEMS'][0][7].")");
-            $this->send($result['ITEMS'][0][2]);
-            $this->send($result['ITEMS'][0][3]);
-            $elements = array();
-            $count = 0;
-            while ($count < 4 && $count < $result['COUNT']) {
-                $elements[] = array(
-                    "title"=>$result['ITEMS'][$count][1]." (".$result['ITEMS'][$count][6].", ".$result['ITEMS'][$count][7].")",
-                    "image_url"=>$result['ITEMS'][$count][2],
-                    "subtitle"=>$result['ITEMS'][$count][3]
-                );
-                $count++;
-            }
-            $this->send("setting elements done");
-            $this->send(sizeof($elements));
-            $this->send(["attachment"=>[
+            $resultResponse = ["attachment"=>[
                 "type"=>"template",
                 "payload"=>[
                     "template_type"=>"list",
-                    "elements"=>$elements
-                ],
+                    "top_element_style"=>"large",
+                    "elements"=>array(),
                 "buttons"=>[
                     [
                         "type"=>'web_url',
                         "url"=>'https://unogs.com/?q='.$parameter,
-                        "title"=>"View Complete Search Result"
+                        "title"=>"View All"
                     ]
                 ]
                 ]
-                ]);
+            ]];
+            $count = 0;
+            while ($count < 4 && $count < $result['COUNT']) {
+                $resultResponse["attachment"]["payload"]["elements"][] = [
+                    "title"=>html_entity_decode(strip_tags($result['ITEMS'][$count][1]), ENT_QUOTES)." (".ucfirst($result['ITEMS'][$count][6]).", ".$result['ITEMS'][$count][7].")",
+                    "image_url"=>$result['ITEMS'][$count][2],
+                    "subtitle"=>html_entity_decode(strip_tags($result['ITEMS'][$count][3]), ENT_QUOTES),
+                    "default_action"=>[
+                        "type"=>"web_url",
+                        "url"=>"https://unogs.com/video/?v=".$result['ITEMS'][$count][4]
+                    ]
+                ];
+                $count++;
+            }
+            if ($count == 1) {
+                $resultResponse["attachment"]["payload"]["template_type"] = "generic";
+                $resultResponse["attachment"]["payload"]["elements"][0]["buttons"] = [
+                    [
+                        "type"=>'web_url',
+                        "url"=>$resultResponse["attachment"]["payload"]["elements"][0]["default_action"]["url"],
+                        "title"=>"View More Details"
+                    ]
+                ];
+                unset($resultResponse["attachment"]["payload"]["top_element_style"]);
+                unset($resultResponse["attachment"]["payload"]["buttons"]);
+            }
+            $this->send($resultResponse);
+            if ($count <  $result['COUNT']){
+                $this->send("Hey ".$this->user->getFirstName().", there were actually ".$result['COUNT']." movies and series that matched \"".$parameter."\". I just returned the 4 most relevant match but you can see them all when you click the \"View All\" button above.");
+            }
         }
     }
 }
