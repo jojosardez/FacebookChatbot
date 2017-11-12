ebeedbcf3e6462faca2fd7cdaf7f14978ae33f7b
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-12 21:51:38 +0800

    Set getSenderId and getAccessToken to public and implemented new sendAction operation.

--- Sender.php
+++ Sender.php
@@ -9,11 +9,11 @@ class Sender {
     protected $senderId;
     protected $accessToken;
 
-    protected function getSenderId() {
+    public function getSenderId() {
         return $this->senderId;
     }
 
-    protected function getAccessToken() {
+    public function getAccessToken() {
         return $this->accessToken;
     }
 
@@ -36,6 +36,32 @@ class Sender {
             $ch = curl_init('https://graph.facebook.com/v2.11/me/messages?access_token='.$this->accessToken);
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($messageToSend));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            $result = curl_exec($ch);
+            curl_close($ch);
+        }
+    }
+
+    public function sendAction($action) {
+        $actionText = '';
+        if ($action == SenderAction::markSeen) {
+            $actionText = 'mark_seen';
+        }
+        else if ($action == SenderAction::typingOn) {
+            $actionText = 'typing_on';
+        }
+        else if ($action == SenderAction::typingOff) {
+            $actionText = 'typing_off';
+        }
+
+        if ($actionText != '') {
+            $actionToSend = [
+                'recipient' => [ 'id' => $this->senderId ],
+                'sender_action' => $actionText
+            ];
+            $ch = curl_init('https://graph.facebook.com/v2.11/me/messages?access_token='.$this->accessToken);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($actionToSend));
             curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
             $result = curl_exec($ch);
             curl_close($ch);
