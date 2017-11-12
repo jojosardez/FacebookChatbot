b4e3e758d9e1735e927609abc7149c910beef419
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-12 19:03:25 +0800

    Contains Karez' implementation of IMDB and PKEDEX commands.

--- ImdbBotCommand.php
+++ ImdbBotCommand.php
@@ -6,18 +6,61 @@ class ImdbBotCommand extends BotCommand {
     }
 
     protected function executeCommand($parameter) {
-        $this->send(["attachment"=>[
-            "type"=>"template",
-            "payload"=>[
-              "template_type"=>"generic",
-              "elements"=>[
-                [
-                  "title"=>$this->command." command is not yet implemented. Parameter passed: ".$parameter,
-                  "image_url"=>"https://cdn.elegantthemes.com/blog/wp-content/uploads/2016/03/500-internal-server-error.jpg",
-                  "subtitle"=>$this->command." command is not yet implemented. Parameter passed: ".$parameter
-                ]
-              ]
-            ]
-          ]]);
+        $this->getMovieDetails(urlencode($parameter));
     }
+
+    protected function getMovieDetails($title) {
+
+        $IMDB_URL = 'http://www.omdbapi.com/?t='.$title.'&apikey=86030ddd';
+
+        if(!empty($title)) {
+            $ch = curl_init($IMDB_URL);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+            $response = curl_exec($ch);
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            // Handle failed query
+            if ($httpcode != 200) {
+                die("Theatres are closed");
+            }
+
+            $imdb = json_decode($response, true);
+
+            if (!empty($imdb) && $imdb["Response"] == "True") {
+                $title = $imdb["Title"]."(".$imdb["Year"].")";
+                $moviedetails = $imdb["Plot"];
+                $poster = $imdb["Poster"];
+                $imdburl = 'www.imdb.com/title/'.$imdb["imdbID"];
+
+                $this->send(["attachment"=>[
+                    "type"=>"template",
+                    "payload"=>[
+                        "template_type"=>"generic",
+                        "elements"=>[
+                            [
+                                "title"=>$title,
+                                "image_url"=>$poster,
+                                "subtitle"=>$moviedetails,
+                                "buttons" => [[
+                                    "type"=>"web_url",
+                                    "url"=> $imdburl,
+                                    "title"=> "View More"
+                                ]]
+                            ]
+                        ]
+                    ]
+                ]]);
+            } else {
+                $this->send("Sorry your movie ".$title." is not found");
+            }
+
+        } else {
+            $this->send("Did you forget to provide the movie title?");
+        }
+
+
+    }
+
 }
